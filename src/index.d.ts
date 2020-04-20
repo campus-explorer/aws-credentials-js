9c1082e090978dcb35f239e3a8625e347e823aac
Stephen Caldwell <steve@campusexplorer.com>
2020-04-19 17:16:50 -0700

    forgot to commit typescript declarations

---
+++ src/index.d.ts
@@ -0,0 +1,20 @@
+export function getProfileCredentials(
+    profile: string,
+    options?: {
+        cacheDir: string,
+        duration: number,
+        getMfaToken: (mfaSerial: string) => Promise<{ token: string }>,
+    }
+): Promise<AWS.Credentials>;
+
+export function getProfileConfig(profile: string): Record<string, string>;
+
+export function useProfile(
+    profile: string,
+    AWS: AWS,
+    options?: {
+        cacheDir: string,
+        duration: number,
+        getMfaToken: (mfaSerial: string) => Promise<{ token: string }>,
+    }
+): Promise<void>;
