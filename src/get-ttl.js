295c2d00629ca3a540e87c6edf827548ee79277b
Stephen Caldwell <scaldwell@gmail.com>
2025-02-19 14:45:53 -0800

    remove all files

--- src/get-ttl.js
+++
@@ -1,7 +0,0 @@
-const adminTtl = 60 * 60 * 1; //  Admin   : 1h
-const normalTtl = 60 * 60 * 8; // Default : 8h
-
-/** @type {(profile: string) => number} */
-const getTtl = (profile) => (profile.endsWith('-admin') ? adminTtl : normalTtl);
-
-module.exports = getTtl;
